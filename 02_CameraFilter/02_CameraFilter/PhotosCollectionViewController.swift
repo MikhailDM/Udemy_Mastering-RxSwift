f77f8daa1bd8e85715edc93cf9d8707e66fce95e
Mike Dmitriev <misha.dmitriev.spb@gmail.com>
2020-05-27 10:46:52 +0300

    Photo Filter App Initial

---
+++ 02_CameraFilter/02_CameraFilter/PhotosCollectionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotosCollectionViewController.swift
+//  02_CameraFilter
+//
+//  Created by Михаил Дмитриев on 27.05.2020.
+//  Copyright © 2020 Mikhail Dmitriev. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotosCollectionViewController: UICollectionViewController {
+    
+}
---
+++ 02_CameraFilter/02_CameraFilter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  02_CameraFilter
+//
+//  Created by Михаил Дмитриев on 27.05.2020.
+//  Copyright © 2020 Mikhail Dmitriev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+
+}
+
